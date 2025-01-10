e8bc983fb3db892c1958649c41a3508eb082ae6a
John Matula <john@johnmatu.la>
2025-01-09 22:14:53 -0700

    Initial port from CodePen

--- src/blocks/navigation/navigation.js
+++ src/blocks/navigation/navigation.js
@@ -1,5 +1,8 @@
 function navigation() {
   return {
+    blah() {
+      console.log("home but will probably show everywhere");
+    }
     /*submenu: false,
     openSubmenu(menu) {
       this.submenu = menu
@@ -7,13 +10,13 @@ function navigation() {
     tester(e) {
       console.log(e)
       console.log(this)
-    },
+    }, x-data="sidebar()"
     isSubmenuOpen(menu) {
       return menu === this.submenu
     },
     closeSubmenus() {
       this.submenu = false
-    },
+    },  
     listenForCloseEvent() {
       // Use ES6 notation so that `this` refers to this component
       // (a proud moment that John remembered this differentiation)
